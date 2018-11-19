b5e7be1daaf0b85bcbb04987f35e703f7cd61943
davidj314 <davidj314@outlook.com>
2018-11-19 10:48:23 -0600

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+var mainNames = []
+var subNames = []
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.substring(0,2) === '$$') {
     	message.reply('pong');
   	}
 });
