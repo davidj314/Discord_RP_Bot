e8d89a307d64a59a07fe5a27c5eb5ec7f0da367b
davidj314 <davidj314@outlook.com>
2018-11-19 11:13:00 -0600

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var mainnames = [];
+var sidenames = [];
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
