99191ef1c874c31ec12748ba22863c99df9aeece
davidj314 <davidj314@outlook.com>
2018-11-20 14:51:01 -0600

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+/*const Discord = require('discord.js');
 const Client = new Discord.Client();
 var pg = require('pg');
 var connection_string="postgres://tfxdiyrtqafcsg:016d85a5be0b32798c3380daf41972fd16c7ace8802f4cc43d95ee42e1bbc319@ec2-54-225-110-156.compute-1.amazonaws.com:5432/dcaet7lhppmpnr"
@@ -63,6 +63,57 @@ Client.on('message', message => {
                 }
                 add_info(info_key, info_content);
                 break;
+        }
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+Client.login(process.env.BOT_TOKEN);
+*/
+
+
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+var pg = require('pg');
+var pg_client = new pg.Client(process.env.DATABASE_URL);
+pg_client.connect();
+var make_table = "CREATE TABLE Info ( ID int NOT NULL, LookupKey varchar(255) NOT NULL, Info_Value varchar(255),PRIMARY KEY (ID));";
+
+pg_client.query(make_table);
+
+    
+    
+var mainnames = [];
+var sidenames = [];
+
+Client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+Client.on('message', message => {
+    if (message.content.substring(0,2) === '$$') {
+    	var args = message.content.substring(2).split(' ');
+        var command = args[0];
+        switch(command){
+            case 'record_name':
+                var name = '';
+                var i;
+                for (i=1;i < args.length; i++){
+                    name+= args[i];
+                }
+                
+                if (name != ''){
+                    mainnames.push(name);
+                    var allNames = '';
+                    var j;
+                    for (j = 0 ; j < mainnames.length ; j++)
+                    {
+                        allNames += mainnames[j] + ' ';
+                    }
+                    
+                    message.channel.send(allNames);
+                }
+                console.log('name sent');
         }
   	}
 });
