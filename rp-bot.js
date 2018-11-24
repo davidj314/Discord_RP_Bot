5f6da664675887554a8f2a62d52a0cb5b3a35ad9
davidj314 <davidj314@outlook.com>
2018-11-24 15:00:50 -0600

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -5,19 +5,7 @@ var connection_string="postgres://tfxdiyrtqafcsg:016d85a5be0b32798c3380daf41972f
 var pg_client = new pg.Client(connection_string);
 pg_client.connect();
 
-function add_info(k, v){
-    var insert_query = "INSERT INTO Info (InfoKey, InfoValue) VALUES($1, $2) RETURNING *";
-    var values = [k, v];
-    pg_client.query(insert_query, values, (err, res) => {
-  if (err) {
-    console.log(err.stack)
-  } else {
-    console.log(res.rows[0])
-    
-  } //end else
-})//end query
-    
-}//end function
+
 
 
 var mainnames = [];
@@ -27,48 +15,32 @@ Client.on('ready', () => {
     console.log('I am ready!');
 });
 
-Client.on('message', message => {
-    if (message.content.substring(0,2) === '$$') {
-    	var args = message.content.substring(2).split(' ');
-        var command = args[0];
-        switch(command){
-            case 'record_name':
-                var name = '';
-                var i;
-                for (i=1;i < args.length; i++){
-                    name+= args[i];
-                }
-                
-                if (name != ''){
-                    mainnames.push(name);
-                    var allNames = '';
-                    var j;
-                    for (j = 0 ; j < mainnames.length ; j++)
-                    {
-                        allNames += mainnames[j] + ' ';
-                    }
-                    
-                    message.channel.send(allNames);
-                }
-                console.log('name sent');
-                break;
-            case 'record_info':
-                if (args.length < 3) return; //must have a key and value following command
-                var info_key = args[1];
-                var info_content = '';
-                var i;
-                for (i=2;i < args.length; i++){
-                    info_content += args[i];
-                }
-                add_info(info_key, info_content);
-                break;
-        }
-  	}
-});
+
 
 // THIS  MUST  BE  THIS  WAY
 Client.login(process.env.BOT_TOKEN);
 */
+
+
+function add_info(k, v){
+    console.log('in the add info function');
+    const  pg  = require('pg');
+    const pg_client = new pg.Client(process.env.DATABASE_URL);
+
+    pg_client.connect();
+    var insert_query = "INSERT INTO Info (InfoKey, InfoValue) VALUES($1, $2) RETURNING *";
+    var values = [k, v];
+    pg_client.query(insert_query, values, (err, res) => {
+  if (err) {
+    console.log(err.stack);
+  } else {
+    console.log(res.rows[0]);
+    
+  } //end else
+})//end query
+    pg_client.end();
+    
+}//end function
 
 
 const Discord = require('discord.js');
@@ -81,13 +53,7 @@ console.log(process.env.DATABASE_URL);
 //var connectionString = "postgres://tfxdiyrtqafcsg:016d85a5be0b32798c3380daf41972fd16c7ace8802f4cc43d95ee42e1bbc319
 //@*HOST*:*PORT*/*DATABASE*"
 
-const  pg  = require('pg');
-const pg_client = new pg.Client(process.env.DATABASE_URL);
 
-pg_client.connect();
-
-const res = pg_client.query('INSERT INTO Info VALUES ("The test", "Test value")' );
-pg_client.end();
 
 
 Client.on('ready', () => {
@@ -100,24 +66,17 @@ Client.on('message', message => {
         var command = args[0];
         switch(command){
             case 'record_name':
-                var name = '';
+                break;
+            case 'record_info':
+                if (args.length < 3) return; //must have a key and value following command
+                var info_key = args[1];
+                var info_content = '';
                 var i;
-                for (i=1;i < args.length; i++){
-                    name+= args[i];
+                for (i=2;i < args.length; i++){
+                    info_content += args[i];
                 }
-                
-                if (name != ''){
-                    mainnames.push(name);
-                    var allNames = '';
-                    var j;
-                    for (j = 0 ; j < mainnames.length ; j++)
-                    {
-                        allNames += mainnames[j] + ' ';
-                    }
-                    
-                    message.channel.send(allNames);
-                }
-                console.log('name sent');
+                add_info(info_key, info_content);
+                break;
         }
   	}
 });
