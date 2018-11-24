49f21f21d3ccc1437232222dfbcd7cf89ce7790d
davidj314 <davidj314@outlook.com>
2018-11-24 17:02:35 -0600

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -71,10 +71,10 @@ client.query('SELECT table_schema,table_name FROM information_schema.tables;', (
   if (err) throw err;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
-  }*/
+  
   client.end();
 });   
-    
+   }*/ 
 }
 
 
