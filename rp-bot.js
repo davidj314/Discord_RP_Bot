89094c781f2b1723d4118177ff74afb9debd0767
davidj314 <davidj314@outlook.com>
2018-11-28 11:29:22 -0600

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -71,7 +71,7 @@ max: 10,
 idleTimeoutMillis: 10000,
 });
 */
-var constring = process.env.DATABASE_URL + "?sslmode=require?ssl=true";
+var constring = process.env.DATABASE_URL + "?ssl=true";
 var MO = new Momo.Client(constring);
 var make_table = "CREATE TABLE Infos ( ID int NOT NULL AUTO_INCREMENT, InfoKey varchar(255) NOT NULL, InfoValue varchar(255) NOT NULL, PRIMARY KEY (ID))";
 console.log('SO SICK OF THIS');
