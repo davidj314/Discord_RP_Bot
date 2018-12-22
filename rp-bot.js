8e2e8f81c9819c7b8bda90f02763e2bbba14ce8a
davidj314 <davidj314@outlook.com>
2018-12-21 19:28:41 -0700

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -270,7 +270,7 @@ Client.on('ready', () => {
 
 Client.on('message', message => {
     //console.log("BEFORE MESSAGE")
-    console.log(message.content);
+    console.log(message.content.substring(0,3));
     if (message.content.substring(0,3) == 'rp!') { 
         console.log("FOUND COMMAND");
         message.channel.send("Found command");
