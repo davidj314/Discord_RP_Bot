00b6755376a6489f3014a2611b655d39f985905e
davidj314 <davidj314@outlook.com>
2018-12-21 18:43:05 -0700

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -284,7 +284,7 @@ Client.on('message', message => {
                 break;
                 
             case 'tester':
-                channel.send("<@!269338190547124235> is a doot");
+                channel.send("!add-moey <@!269338190547124235> 19");
                 break;
                 
             case 'id':
