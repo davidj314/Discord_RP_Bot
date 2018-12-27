223cadaf0263bd1338681f05b7388a037e5ce2a6
davidj314 <davidj314@outlook.com>
2018-12-27 01:00:55 -0700

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -422,7 +422,9 @@ Client.on('message', message => {
                 for (i=0;i < m_array.length; i++){
                     console.log(m_array[i].content);
                     if (found_bump){
-                        if (!m_array[i].content.match(/!disboard +(B|b)(U|u)(M|m)(P|p)/)continue;
+                        if (!m_array[i].content.match(/!disboard +(B|b)(U|u)(M|m)(P|p)/){
+                            continue;
+                            }
                         console.log(m_array[i].author.username + " is " + m_array[i].author.id);
                         i = m_array.length;
                     }
