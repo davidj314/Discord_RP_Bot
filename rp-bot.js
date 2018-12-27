11cba504e4a27e0304ecb02a78e0e5e0a831282b
davidj314 <davidj314@outlook.com>
2018-12-26 23:55:16 -0700

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -413,7 +413,7 @@ Client.on('message', message => {
             var prev_id = parseInt(message.id) - 1;
             //convert to string?
             message.channel.fetchMessages({ limit: 2 })
-            .then(messages => {messages.forEach(function(element) {console.log(element.content);}); }   )
+            .then(messages => {messages.forEach(function(element) {console.log(element.author.username);}); }   )
             .catch(console.error);
             console.log('past the fetch');
         }
