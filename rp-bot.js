c9c83207b30661e1efbef59ee20888cc16a05845
davidj314 <davidj314@outlook.com>
2019-01-19 04:50:39 -0700

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -631,7 +631,7 @@ Client.on('message', message => {
                 var role = args[3];
                 //insert_new_trigger_message(server_id, channel_id, message_id, emoji, role, callback)
                 convert_role_to_snowflake(message.guild, role, (snowflake)=>{ 
-                    insert_new_trigger_message(guild_id, channel, message_id, trigger, snowflake, (msg)=>{
+                    insert_new_trigger_message(guild_id, channel.id, message_id, trigger, snowflake, (msg)=>{
                         channel.send(msg)
                     })
                 }, ()=>{channel.send("Failed to make trigger")});
