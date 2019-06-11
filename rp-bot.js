62504ce433382003cd7ea3f97212bf00be331138
davidj314 <davidj314@outlook.com>
2019-06-11 15:39:20 -0700

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -780,6 +780,7 @@ Client.on('message',  async message => {
 	// Select the style that will be used to fill the text in
 	ctx.fillStyle = '#ffffff';
 	ctx.strokeStyle = 'black';
+	ctx.lineWidth = 1; 
 	// Actually fill the text with a solid color
 	ctx.fillText("   4   \n4    4\n   4", 55, 35);
 	ctx.fillText("   4   \n4    4\n   4", 55, 155);
