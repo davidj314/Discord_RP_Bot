02132eefbc0d186af8342a1a57624f54030c928a
davidj314 <davidj314@outlook.com>
2019-06-14 00:19:12 -0700

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -1209,6 +1209,10 @@ Client.on('message',  async message => {
 						message.channel.send("It is not your turn.")
 						return;
 					}
+					if(board[temp].positions[[d1][d2] != -1){
+						message.channel.send("There's already a card there")
+						return;
+					}
 					board[temp].positions[d1][d2] = card;
 					hands[pointer].hand[card_index-1].used = 1;
 					found++;
