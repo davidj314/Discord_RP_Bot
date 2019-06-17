8fca3090eff17adccff3f6280c6bb7faa228e334
davidj314 <davidj314@outlook.com>
2019-06-16 17:20:51 -0700

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -1387,7 +1387,7 @@ Client.on('message',  async message => {
 		get_all_cards(guild_id, (rows)=>{
 			var output = "CID         Total               Up               Left          Right           Down               Name   \n";
 			var allcards = []
-			rows.forEach(function(row){ allcards.push({cid: row.cid, up: row.upval, down: row.downval, left: row.leftval, right: row.rightval, name: row.name, total: row.upval+row.downval+row.leftval+row.rightval})});
+			rows.forEach(function(row){ allcards.push({cid: row.char_id, up: row.upval, down: row.downval, left: row.leftval, right: row.rightval, name: row.name, total: row.upval+row.downval+row.leftval+row.rightval})});
 			console.log(allcards);
 			for(var i = 0; i < allcards.length; i++){
 				output += allcards[i].cid;
