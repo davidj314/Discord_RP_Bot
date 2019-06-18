9dddaddf1c2baac28dd0f56c36e143b1298acdf5
davidj314 <davidj314@outlook.com>
2019-06-17 22:50:10 -0700

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -551,7 +551,7 @@ function pop_pack(server_id, user_id, callback, bad)
 	var select_query = "SELECT Packs FROM Packs WHERE server_id = $1 AND user_id = $2";
 	var values = [server_id, user_id];
     	var pool = new PG.Pool({ connectionString: process.env.DATABASE_URL, SSL: true});
-    	pool.query(select_query, (err, result) => {
+    	pool.query(select_query, values, (err, result) => {
 		console.log(result);
 		if (err) {
 		    console.log('error occurred');
