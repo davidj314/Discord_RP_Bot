811c3f838d724d7badb3806ba1cb477157097b99
davidj314 <davidj314@outlook.com>
2019-06-17 21:47:56 -0700

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -1306,7 +1306,7 @@ Client.on('message',  async message => {
     disboard_check(message);
 	
 	
-    if (5 > 1)get_training(message.guild.id, message.author.id, (char_id)=>{  get_card_info(message.guild.id, char_id, (card)=>{   
+    if (5 < 1)get_training(message.guild.id, message.author.id, (char_id)=>{  get_card_info(message.guild.id, char_id, (card)=>{   
 
 	var points = card.upval + card.downval + card.leftval + card.rightval;
 	if (points >= 36) return;
