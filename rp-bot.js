7a61bbb6a183c4ee54b62c14fa778613d465656f
davidj314 <davidj314@outlook.com>
2019-06-24 18:33:53 -0700

    Update rp-bot.js

--- rp-bot.js
+++ rp-bot.js
@@ -1,5 +1,5 @@
 var Discord = require('discord.js');
-var Tester = require('Cardd');
+var Tester = require('Carde');
 var Client = new Discord.Client();
 const Canvas = require('canvas');
 var PG = require('pg');
